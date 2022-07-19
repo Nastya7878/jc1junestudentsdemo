8cb9c0955579ea070db2826b81ae9fa14ce3393c
n_verem <buraya82@gmail.com>
2022-07-19 14:12:18 +0300

    Homework2

--- Homework2/src/CarMovies.java
+++
@@ -1,6 +0,0 @@
-public interface CarMovies {
-  double start();
-  double stop();
-  double distance(double time, double speed);
-
-}
--- Homework2/src/UtilClass.java
+++
@@ -1,8 +0,0 @@
-public class UtilClass {
-
-
-  public static Car[] setNumberOfArray(int Count) {
-    return new Car[Count];
-  }
-
-}
